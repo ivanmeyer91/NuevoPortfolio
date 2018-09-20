e67bce0f1829885498f5b2d15cb10d107faad996
ivan <ivanmeyer1991@gmailcom>
2018-09-19 23:36:08 -0300

    cambio

---
+++ js/botones.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+function instagram(){
+    location.href ="https://www.instagram.com/ivan_meyer_/";
+};
+
+
+function github(){
+    location.href = "https://github.com/ivanmeyer91";
+};
+
+function twitter(){
+    location.href = "https://twitter.com/ivanmeyer_";
+};
+
+function facebook(){
+    location.href = "https://www.facebook.com/ivan.meyer3";
+};
+
+function formulario(){
+    location.href = "https://ivanmeyer91.github.io/formulario.io/";
+};
+
+function apptiempo(){
+    location.href = "https://github.com/ivanmeyer91/appclima";
+}
